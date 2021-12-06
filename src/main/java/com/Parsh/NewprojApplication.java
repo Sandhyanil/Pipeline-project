7205841b502a38fb223ce23b98c135b1f64b810a
Sandhyanil <parsh@LAPTOP-B2O9T98P>
2021-12-06 21:59:48 +0530

    First commit

--- src/main/java/com/Parsh/NewprojApplication.java
+++ src/main/java/com/Parsh/NewprojApplication.java
@@ -2,9 +2,10 @@ package com.Parsh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NewprojApplication {
+public class NewprojApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewprojApplication.class, args);
